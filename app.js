18855826f82a5560cfce8fb102dc741e32582a35
yania-pena <yania.pena@udla.edu.ec>
2022-07-10 23:59:55 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var createError = require('http-errors');
 var express = require('express');
+var https=require ('https');
+var fs=require('fs');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -20,8 +22,8 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({limit:'12mb'}));
+app.use(bodyParser.urlencoded({ limit:'12mb',extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(session({ 
@@ -41,9 +43,40 @@ app.use('/horarios',horariosRouter);
  // next(createError(404));
 //});
 // error handler
-const PORT = process.env.PORT || 4000
-app.listen(PORT, function() {
-  console.log("Servidor escuchando en el puerto",PORT)
- 
+var credentials = {
+  key: fs.readFileSync ('./cert/key.key') ,
+  cert: fs.readFileSyne ('./cert/cert.crt'),
+  ca: fs.readFileSync ('./cert/bundle.crt'),
+  requestCert: true,
+  rejectUnauthorized: false
+}
+
+io.on('connection', function (socket) {
+  console. log (socket);
+  socket. on ('authenticate', function (data) {
+  console. log (data);
+  });
 });
+
+
+var server = https.createServer(credentials,app);
+var io=require('socket.io')(server);
+
+var port = process.env.PORT || 8080;
+server.listen (port, function (){
+console. log ("server listen on", this .address ());
+});
+server.on ( 'clientError' , function (err) {
+console.log( 'ERROR', err);
+});  
+
+
+
+
+
+//const PORT = process.env.PORT || 4000
+//app.listen(PORT, function() {
+  //console.log("Servidor escuchando en el puerto",PORT)
+ 
+//});
 module.exports = app;
